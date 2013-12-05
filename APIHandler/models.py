32f9860a02f12146e3487b56a7e8456449c57e62
reykjanes <reykjanes@reykjanes-w>
2013-12-06 00:34:20 +0100

    [FIXED] 	- .gitignore fixed 	- Caracteristic tuple is fixed (added value) [ADDED] 	- User class
    Signed-off-by: reykjanes <reykjanes@reykjanes-w>

--- APIHandler/models.py
+++ APIHandler/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 
 #---------------------------------------------#
-# Notes : 
+# Notes : Caracteristic Class
 # Status : 
 # Test :   
 class Caracteristic(models.Model):
     label = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
 
-#---------------------------------------------#
-# Notes : 
+#-----------------------------------------------------------------------------#
+# Notes : Product Class
 # Status : 
 # Test :
+#//TODO: finish the documentation
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     caracteristics = models.ManyToManyField(Caracteristic,
                                             related_name='products')
+#-----------------------------------------------------------------------------#
+# Notes : User class
+# Status : 
+# Test :
+#//TODO: finish the documentation
+class User(models.Model):
+    name = models.CharField(max_length=80)
+    boughtProducts = models.ManyToManyField(Product,related_name='User')
+    
+
+    
