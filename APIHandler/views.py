2ab52e518da4e3c5ba3182f0f1f59049a4fc6c4f
EpicSound <david.goncalves@iut-valence.fr>
2013-12-05 22:12:04 +0100

    Updated APIHandler:views.py
    Added ugly request handler

--- APIHandler/views.py
+++ APIHandler/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.utils import simplejson
+
+
 
 # Create your views here.
+def item(request):
+    some_data_to_dump = {
+       'id': 'foo',
+       'nom': 'bar',
+       'carac0': 'carac0',
+       'carac1': 'carac1',
+       'carac2': 'carac2',
+       'carac3': 'carac3',
+       'carac4': 'carac4',
+       'carac5': 'carac5',
+       'carac6': 'carac6',
+       'carac7': 'carac7',
+       'carac8': 'carac8',
+       'carac9': 'carac9',
+    }
+    
+    data = simplejson.dumps(some_data_to_dump)
