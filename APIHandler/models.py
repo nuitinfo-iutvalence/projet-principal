0c9b6dc1a94a447721926bb3a5d2f774db5aa5a1
EpicSound <david.goncalves@iut-valence.fr>
2013-12-05 21:38:49 +0100

    Updated APIHandler:models.py
    Added simple "Produit" class.

--- APIHandler/models.py
+++ APIHandler/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Produit(models.Model):
+    id = models.IntegerField();
+    nom = models.CharField(max_length=200)
+    
+    carac0 = models.CharField(max_length=200)
+    carac1 = models.CharField(max_length=200)
+    carac2 = models.CharField(max_length=200)
+    carac3 = models.CharField(max_length=200)
+    carac4 = models.CharField(max_length=200)
+    carac5 = models.CharField(max_length=200)
+    carac6 = models.CharField(max_length=200)
+    carac7 = models.CharField(max_length=200)
+    carac8 = models.CharField(max_length=200)
+    carac9 = models.CharField(max_length=200)
+    
+    
+    prix = models.IntegerField();
