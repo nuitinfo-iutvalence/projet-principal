22fb0148d2dfc7ba2dce8b291681d8cda286daa2
EpicSound <david.goncalves@iut-valence.fr>
2013-12-06 00:25:04 +0100

    Updated APIHandler:admin.py

--- APIHandler/admin.py
+++ APIHandler/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from APIHandler.models import Product
+from APIHandler.models import Caracteristic
 
 admin.site.register(Product)
+admin.site.register(Caracteristic)
 # Register your models here.
