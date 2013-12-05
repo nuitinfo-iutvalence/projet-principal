ad58992983561c904c683b7ee16aa5ee08873624
EpicSound <david.goncalves@iut-valence.fr>
2013-12-06 00:17:12 +0100

    Updated views according to the new models

--- APIHandler/views.py
+++ APIHandler/views.py
@@ -1,12 +1,14 @@
+from django.core import serializers
 from django.http.response import HttpResponse
 from django.utils import simplejson
-from APIHandler.models import Produit 
-from django.core import serializers
+
+from APIHandler.models import Product
+
 
 def item(request):
    
 
-    data = serializers.serialize("json", Produit.objects.all())
+    data = serializers.serialize("json", Product.objects.all())
     
     
     return HttpResponse(data, mimetype='application/json')
