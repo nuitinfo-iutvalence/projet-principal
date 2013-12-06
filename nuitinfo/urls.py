6bcc5a0b8db928e0f494ce0cd99ae68adc357bdd
EpicSound <david.goncalves@iut-valence.fr>
2013-12-06 05:44:32 +0100

    Updated urls.py

--- nuitinfo/urls.py
+++ nuitinfo/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nuitinfo.views.home', name='home'),
-    url(r'^$', include('APIHandler.urls')),
+    url(r'^', include('APIHandler.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT
  ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
