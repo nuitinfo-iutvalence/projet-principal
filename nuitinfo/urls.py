a9ef81a56b44f480bec81ec2584413aab626ac1c
EpicSound <david.goncalves@iut-valence.fr>
2013-12-06 06:59:16 +0100

    Uploaded index.html and updated urls.py

--- nuitinfo/urls.py
+++ nuitinfo/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import  TemplateView
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,6 +13,7 @@ urlpatterns = patterns('',
 
     url(r'', include('APIHandler.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT
  ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
