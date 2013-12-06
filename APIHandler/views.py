3b37933abb33e11ef6d48b9e748a8854ccdb1d7a
EpicSound <david.goncalves@iut-valence.fr>
2013-12-06 05:44:11 +0100

    Updated views

--- APIHandler/views.py
+++ APIHandler/views.py
@@ -1,8 +1,9 @@
-from django.http.response import HttpResponse
 from django.http import Http404
+from django.http.response import HttpResponse
+import json
 
-from APIHandler.models import Product
-from APIHandler.models import Category 
+from APIHandler.models import Caracteristic, Category, Product
+
 
 #-----------------------------------------------------------------------------#
 # Notes : Function sending the json's product
@@ -28,3 +29,27 @@ def send_category(request, pk):
     jsonData = category.to_json()
     
     return HttpResponse(jsonData, mimetype='application/json')
+
+def answer_search(request):
+    q = request.GET['q']
+    keywords = q.split()
+    
+    prod_found = Product.objects.filter(name__in=keywords)
+        
+    data = [p.to_dict() for p in prod_found]
+        
+    return HttpResponse(json.dumps(data), mimetype='application/json')
+        
+        
+            
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
