032a388d2e70320103a962a75978f42e61305499
Reykjanes <vincent.guilleminot@iut-valence.fr>
2013-12-06 03:58:35 +0100

    ninja commit

--- APIHandler/admin.py
+++ APIHandler/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from APIHandler.models import Product
 from APIHandler.models import Caracteristic
 from APIHandler.models import User
+from APIHandler.models import Category
 
 #Adding models to admin panel
 admin.site.register(Product)
 admin.site.register(Caracteristic)
 admin.site.register(User)
+admin.site.register(Category)
 
 # Register your models here.
